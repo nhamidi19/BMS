681d242dec103435f4615d75db40f626aad61099
salman-hamidi2024 <52631444+sadeqsh66@users.noreply.github.com>
2025-03-27 17:19:45 +0330

    xml downloader created

--- src/Mohsenin/xml_maker.py
+++ src/Mohsenin/xml_maker.py
@@ -2,27 +2,29 @@
 import xml.etree.ElementTree as ET
 from django.http import HttpResponse
 
-def convert_xml(table):
+def convert_xml(table, objects=None):
     # ایجاد ریشه XML
-    root = ET.Element(table._meta.model_name)  # استفاده از نام مدل به عنوان ریشه
+    root = ET.Element(table._meta.model_name)
 
-    # دریافت همه داده‌های مدل
-    objects = table.objects.all()
-    
+    # اگر اشیایی ارسال نشده یا لیست خالی است، تمام داده‌ها را بگیرید
+    if objects is None or not objects.exists():
+        objects = table.objects.all()
+
     for obj in objects:
-        # عنصر اصلی برای هر شیء
-        obj_element = ET.SubElement(root, table._meta.model_name)
+        # ایجاد عنصر اصلی (row) برای هر شیء
+        obj_element = ET.SubElement(root, "row")  # تغییر نام عنصر به "row"
 
         # دریافت فیلدهای مدل به صورت دینامیک
         for field in table._meta.fields:
             field_name = field.name
             field_value = getattr(obj, field_name, "")
             ET.SubElement(obj_element, field_name).text = str(field_value)
-
+    
     # تبدیل داده‌های XML به رشته
     tree = ET.ElementTree(root)
-    response = HttpResponse(content_type='application/xml')  # تغییر نوع محتوا به application/xml
-    response['Content-Disposition'] = f'attachment; filename="{table._meta.model_name}.xml"'  # تنظیم نام فایل خروجی
+    response = HttpResponse(content_type='application/xml')
+    response['Content-Disposition'] = f'attachment; filename="{table._meta.model_name}.xml"'
     tree.write(response, encoding='unicode')
 
     return response
+
